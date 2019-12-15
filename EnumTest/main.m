8f042493e675190122a83428e2e8fa1946532015
Tianid <tianid73@gmail.com>
2019-12-15 15:32:23 +0300

    Initial Commit

---
+++ EnumTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EnumTest
+//
+//  Created by Tianid on 15.12.2019.
+//  Copyright © 2019 Tianid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
